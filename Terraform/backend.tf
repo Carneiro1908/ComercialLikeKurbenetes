b6a41db42b40d822395437fda4d102ebd293f09b
Carneiro1908 <tomasclima1406@gmail.com>
2026-06-29 10:32:44 +0100

    Fix: backend.tf syntax

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   # Commercial Remote Backend Configuration
   backend "s3" {
-    bucket         = "comercial-k8s-protected-storage-2026"
+    bucket         = "comercial-k8s-protected-storage-tomas-2026"
     key            = "infrastructure/terraform.tfstate" # Path inside the bucket
     region         = "eu-north-1"
     encrypt        = true
